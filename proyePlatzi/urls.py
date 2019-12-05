8caaaa70e61d314ff20f72ae2c38ae644ce771c9
Karen Quiceno <ksquiceno3@misena.edu.co>
2019-12-05 12:25:42 -0500

    Vimos orm de Django bd

--- proyePlatzi/urls.py
+++ proyePlatzi/urls.py
@@ -5,7 +5,7 @@ from proyePlatzi import views as local_views
 from posts import views as posts_views
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('hello-world/', local_views.hello_world),
     path('sorted', local_views.sort_integers),
     path('hi/<str:name>/<int:age>', local_views.say_hi),
